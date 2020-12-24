b249d5285b999a0c0a02a7f5b0d9a1cf15ab1746
vinit-churi <vinitchuri@hotmail.com>
2020-12-24 21:12:07 +0530

    done with the final project

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import sanityClient from "../client";
+import developer from "../developer.jpg";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 function About() {
-  return <h2>About us page</h2>;
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type== "author"]{
+        name,
+        bio,
+        "authorImage":image.asset->url
+      }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  }, []);
+
+  if (!author) return <div>Loading..</div>;
+
+  return (
+    <main className="relative">
+      <img src={developer} alt="developer" className="absolute w-full" />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+          <img
+            src={urlFor(author.authorImage).url()}
+            alt={author.name}
+            className="rounded w-32 lg:w-64 lg:h-64 mr-8"
+          />
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="text-green-100">{author.name}</h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent
+                blocks={author.bio}
+                projectId="8e6ncrws"
+                dataset="production"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default About;
